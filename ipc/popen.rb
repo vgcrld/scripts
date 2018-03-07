e7405b3fb40c686df4bbf1ca776c826c173ab33f
Rich Davis <vgcrld@gmail.com>
2018-03-07 17:25:12 -0500

    Finally got a TSM query that connects.

--- ipc/popen.rb
+++ ipc/popen.rb
@@ -7,64 +7,72 @@ include Open3
 def connect 
   @output = Tempfile.new(['tsmoutput-','.tmp'])
   @output.sync = true
-  @size = @output.size
   @outs ||= []
   @outs << @output
-  @stdin, @stdout, @stderr, @pid = popen3("dsmadmc -id=admin -pa=admin -display=list -dataonly=yes > #{@output.path}")
+  @stdin, @stdout, @stderr, @pid = popen3("dsmadmc -id=admin -pa=admin -display=list -dataonly=yes -alwaysprompt -NEWLINEAFTERPrompt > #{@output.path}")
+end
+
+def get_delim
+  delim = nil
+  while delim.nil? or delim.empty?
+    delim = get_buffered('.*:.*>\n')
+  end
+  return delim
+end
+
+def close_all
+  @stdin.close  unless @stdin.nil?
+  @stdout.close unless @stdout.nil?
+  @stderr.close unless @stderr.nil?
 end
 
 def exec(cmd)
-  @size = @output.size
-  puts "Execute: #{cmd}"
+  ret = Hash.new
+  ret[cmd] = Hash.new
   begin
     @stdin.puts(cmd)
   rescue
     puts "Failed, reconnecting: #{fetch(@stderr)}"
-    connect
-    exec(cmd)
+    retry
   end
-  return @output
+  data = get_buffered
+  ret[cmd][:format] = format(data)
+  ret[cmd][:raw] = data
+  return ret
 end
 
-def results
-  fetch(@output)
+def get_buffered(delim=@delim)
+  data = ""
+  until data.match /#{delim}$/
+    data += @output.read_nonblock(@output.size-@output.pos)
+  end
+  return data
 end
 
-def fetch(stream)
-  begin
-    stream.flush
-    result = []
-    wait_for_output
-    result += stream.each_line.map{ |x| x.chomp.strip.squeeze(" ").split(": ",2) }
-    @size = @output.size
-  rescue
-    result = nil
+def format(data,delim=@delim)
+  data = data.gsub(delim,"").squeeze("\n")
+  data.lines.map do |line|
+    line.strip.squeeze(" ").split(": ",2)
   end
-  return result
 end
 
-def wait_for_output
-  cycles = 0
-  while @output.size == @size 
-    cycles += 1
-    print '.' if cycles % 10000 == 0
-    break if cycles > 1000000
-  end
+def reopen
+  close_all
+  connect
 end
 
 connect 
 
+@delim = get_delim
+
 puts @output.path
 
-#exec('q db'); wait_for_output; o=fetch(@output); @output.rewind
+x = exec('q db')
 
 require 'debug'
 
-@stdin.close unless @stdin.nil?
-@stdout.close  unless @stdout.nil?
-@stderr.close unless @stderr.nil?
-
 END{
+  close_all
   @outs.each{ |file| puts "Deleted: #{file.path}"; file.unlink }
 }
 
