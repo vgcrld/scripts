4b755a996a83db0a2330114f2295016d26def8f6
Rich Davis <vgcrld@gmail.com>
2018-09-18 08:39:25 -0400

    updated sinatra

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -1,14 +1,30 @@
 require 'sinatra'
+require 'awesome_print'
+require 'haml'
 
 get "/" do
   "Hello World"
 end
 
 get "/tsm/query/:object" do
-  obj = params[:object]
-  "q #{obj} f=d"
+  haml :tsm
 end
 
 get "/:customer/data" do |o|
   "Hello, #{o}! How are you today?"
 end
+
+get "/:username" do
+  ap params
+  return "#{params}"
+end
+
+get "/user/:username" do
+  haml :index
+end
+
+post "/user/:username" do
+  payload = JSON.parse(request.body.read)
+  return "We got your data #{payload["name"]}"
+end
+
