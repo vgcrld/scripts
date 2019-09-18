7ce1b61de06423a14005c625def713ae36b9bd63
Rich Davis <rdavis@galileosuite.com>
2019-09-18 21:13:53 +0000

    add gpe_process_get.rb

---
+++ gpe_process_get.rb
@@ -0,0 +1,46 @@
+#!/bin/env ruby
+
+require 'yaml'
+require 'awesome_print'
+
+@db = YAML.load(File.new('data.yaml','r'))
+
+def types
+  return @db.values.map{ |o| o['type'].values }.flatten.uniq
+end
+
+def type(customer,type)
+  types = @db[customer]['type'].values
+  return types.map.each_with_index{ |o,i| i if o == type }.compact
+end
+
+def get(customer,type)
+  idx = type(customer,type)
+  return @db[customer]['type'].keys.values_at(*idx)
+end
+
+def customers
+  return @db.keys
+end
+
+## type = 'ds8k'
+## type = 'oracle'
+## type = 'pure'
+## type = 'flash'
+## type = 'vmware'
+## type = 'epic'
+
+types.each do |type|
+  define_method :"get_#{type}" do |cust|
+    sys = Hash.new
+    sys.store(cust,get(cust, type))
+  end
+end
+
+sys = Hash.new
+customers.each do |cust|
+  systems = get_vmware(cust)
+  sys.store(cust,systems) unless systems.empty?
+end
+
+ap sys
