48e09f0a03c0b76fa0213b60d7b6007224213351
Rich Davis <rdavis@galileosuite.com>
2020-05-28 13:51:36 +0000

    save from karl

---
+++ gets3.sh
@@ -0,0 +1,15 @@
+###D aws s3 cp --recursive s3://etl-x2gpe.galileosuite.com/Analysis_Group/cc83aceacf83e6119c673cfdfe02e7c2/2020/03/ /share/prd01/rdavis/ag/03
+###D aws s3 cp --recursive s3://etl-x2gpe.galileosuite.com/Analysis_Group/cc83aceacf83e6119c673cfdfe02e7c2/2020/04/ /share/prd01/rdavis/ag/04
+
+###D aws s3 cp --recursive s3://etl-x2gpe.galileosuite.com/DandH/5ab25322eb0ce811a58e7cd30aea05f0/2020/03/ /share/prd01/rdavis/DandH/03
+###D aws s3 cp --recursive s3://etl-x2gpe.galileosuite.com/DandH/5ab25322eb0ce811a58e7cd30aea05f0/2020/04/ /share/prd01/rdavis/DandH/04
+
+#aws s3 cp --recursive s3://etl-x2gpe.galileosuite.com/VWR/a64fbe739e9de511a5f80025b50a006d/2020/ ~/gpe/vwr/
+#aws s3 cp --recursive s3://etl-x2gpe.galileosuite.com/VWR/b64c1f141101e811959b0025b5a4005d/2020/ ~/gpe/vwr/
+#aws s3 cp --recursive s3://etl-x2gpe.galileosuite.com/VWR/60e527c0992de611bc580025b50401ff/2020/ ~/gpe/vwr/
+
+###D aws s3 cp --recursive s3://etl-x2gpe.galileosuite.com/atsgroup/d60b21da185e11e682260894ef035c20/2020/03/ /share/prd01/rdavis/atsgroup/03/
+aws s3 cp --recursive s3://etl-x2gpe.galileosuite.com/atsgroup/d60b21da185e11e682260894ef035c20/2020/04/ /share/prd01/rdavis/atsgroup/04/
+
+#aws s3 cp --recursive s3://etl-x2gpe.galileosuite.com/Dept_REV/f2d2bef01af811df8bd9009a01040000/2020/ ~/gpe/rev/
+#aws s3 cp --recursive s3://etl-x2gpe.galileosuite.com/Dept_LI/a46df78a0f3eea11acb33868dd1ca649/2020/ ~/gpe/li/
