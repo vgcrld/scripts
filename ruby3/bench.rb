a937ec4ca3284f3f393cc7f749013dfd965eace3
Rich Davis <rdavis@ip-192-168-1-55.ec2.internal>
2021-05-18 11:13:47 -0400

    bench

--- ruby3/bench.rb
+++ ruby3/bench.rb
@@ -3,15 +3,24 @@ def tarai(x, y, z) =
   x <= y ? y : tarai(tarai(x-1, y, z),
                      tarai(y-1, z, x),
                      tarai(z-1, x, y))
-require 'benchmark'
-Benchmark.bm do |x|
-  # sequential version
-  x.report('seq'){ 4.times{ tarai(14, 7, 0) } }
+# require 'benchmark'
+# Benchmark.bm do |x|
+#   # sequential version
+#   x.report('seq'){ 4.times{ tarai(14, 7, 0) } }
 
-  # parallel version
-  x.report('par'){
-    4.times.map do
-      Ractor.new { tarai(14, 7, 0) }
-    end.each(&:take)
-  }
-end
+#   # parallel version
+#   x.report('par'){
+#     4.times.map do
+#       Ractor.new { tarai(14, 7, 0) }
+#     end.each(&:take)
+#   }
+# end
+
+
+
+def hello(name) = 
+  puts("hello, #{name}!")
+
+
+
+hello "rich"
