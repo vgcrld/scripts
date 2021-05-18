c6d56a4d0078a8e983298841ed5f0bf59afebebd
Rich Davis <rdavis@galileosuite.com>
2021-05-18 16:14:16 -0400

    udpate

---
+++ prim-ds8k-get.sh
@@ -0,0 +1,4 @@
+aws --profile prod s3 --recursive cp s3://etl-x2gpe.galileosuite.com/Prime_Therapeutics/IBM-2107-75HFN31/2021/05/18/ ~/gpe/ds8k-for-bob/
+aws --profile prod s3 --recursive cp s3://etl-x2gpe.galileosuite.com/Prime_Therapeutics/IBM-2107-75HGH81/2021/05/18/ ~/gpe/ds8k-for-bob/
+aws --profile prod s3 --recursive cp s3://etl-x2gpe.galileosuite.com/Prime_Therapeutics/IBM-2107-75KDR91/2021/05/18/ ~/gpe/ds8k-for-bob/
+aws --profile prod s3 --recursive cp s3://etl-x2gpe.galileosuite.com/Prime_Therapeutics/IBM-2107-75KHP71/2021/05/18/ ~/gpe/ds8k-for-bob/
--- s3.sh
+++
@@ -1,4 +0,0 @@
-aws --profile prod s3 ls s3://etl-x2gpe.galileosuite.com/Prime_Therapeutics/IBM-2107-75HFN31/2021/05/18/vwshmc002p.20210518.183001.GMT__048509__.IBM-2107-75HFN31.ds8k.gz
-aws --profile prod s3 ls s3://etl-x2gpe.galileosuite.com/Prime_Therapeutics/IBM-2107-75HGH81/2021/05/18/usmnd01shmc001d.20210518.183001.GMT__204030__.IBM-2107-75HGH81.ds8k.gz
-aws --profile prod s3 ls s3://etl-x2gpe.galileosuite.com/Prime_Therapeutics/IBM-2107-75KDR91/2021/05/18/usmnd01shmc0002d.20210518.190001.GMT__723943__.IBM-2107-75KDR91.ds8k.gz
-aws --profile prod s3 ls s3://etl-x2gpe.galileosuite.com/Prime_Therapeutics/IBM-2107-75KHP71/2021/05/18/usmnd02shmc0002d.20210518.183001.GMT__102436__.IBM-2107-75KHP71.ds8k.gz
