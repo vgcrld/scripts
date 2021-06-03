db804e3325043795e1f1087d5934a0f4ecf6c0ed
Rich Davis <rdavis@galileosuite.com>
2021-06-03 13:20:20 -0400

    primesuite

---
+++ s3prime.sh
@@ -0,0 +1,8 @@
+aws --profile prod --recursive s3 cp s3://etl-x2gpe.galileosuite.com/Prime_Therapeutics/IBM-2107-75KDR91/2021/05/24 ~/gpe/prime-weekend/
+aws --profile prod --recursive s3 cp s3://etl-x2gpe.galileosuite.com/Prime_Therapeutics/IBM-2107-75KHY01/2021/05/24 ~/gpe/prime-weekend/
+aws --profile prod --recursive s3 cp s3://etl-x2gpe.galileosuite.com/Prime_Therapeutics/IBM-2107-75GKF41/2021/05/24 ~/gpe/prime-weekend/
+aws --profile prod --recursive s3 cp s3://etl-x2gpe.galileosuite.com/Prime_Therapeutics/IBM-2107-75KBM71/2021/05/24 ~/gpe/prime-weekend/
+aws --profile prod --recursive s3 cp s3://etl-x2gpe.galileosuite.com/Prime_Therapeutics/IBM-2107-75HFN31/2021/05/24 ~/gpe/prime-weekend/
+aws --profile prod --recursive s3 cp s3://etl-x2gpe.galileosuite.com/Prime_Therapeutics/IBM-2107-75HDN61/2021/05/24 ~/gpe/prime-weekend/
+aws --profile prod --recursive s3 cp s3://etl-x2gpe.galileosuite.com/Prime_Therapeutics/IBM-2107-75HGH81/2021/05/24 ~/gpe/prime-weekend/
+aws --profile prod --recursive s3 cp s3://etl-x2gpe.galileosuite.com/Prime_Therapeutics/IBM-2107-75KHP71/2021/05/24 ~/gpe/prime-weekend/
