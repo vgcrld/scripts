91cd9ed48aa2d6bf495a79b4068520cbeb62bac6
Rich Davis <rdavis@galileosuite.com>
2021-06-04 16:02:17 -0400

    updates

--- gets3primetoday.sh
+++ gets3primetoday.sh
@@ -7,10 +7,15 @@ month=$(date +'%m')
 day=$(date +'%d')
 year=$(date +'%Y')
 
+
+# for uuid in $(aws s3 --profile prod ls  s3://etl-x2gpe.galileosuite.com/Prime_Therapeutics/ | awk '/75LFB21|75GKF41|75KHY01|75LFR91|75HFN31|75HDN61|75KBM71|75HGH81|75KHP71|75KDR91/{print $2}')
 for uuid in $(aws s3 --profile prod ls  s3://etl-x2gpe.galileosuite.com/Prime_Therapeutics/ | awk '{print $2}')
 do
     prefix="s3://etl-gpe.galileosuite.com/Prime_Therapeutics/${uuid}${year}/${month}/${day}/"
-    latest_file=$(aws s3 --profile prod ls "${prefix}" | awk 'END{print $4}')
-    recall_cmd="aws s3 --profile prod cp ${prefix}${latest_file} ~/gpe/primetoday/"
-    eval "${recall_cmd}"
+    for i in $(aws s3 --profile prod ls "${prefix}" | tail -4 | awk '{print $4}')
+    do
+      recall_cmd="aws s3 --profile prod cp ${prefix}${i} ~/gpe/primetoday/"
+      echo "Recover: '$i'"
+      echo "${recall_cmd}" >> ~/code/scripts/restore_prime_today.sh
+    done
 done
